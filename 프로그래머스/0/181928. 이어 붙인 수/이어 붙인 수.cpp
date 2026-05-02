6910feaaf32f06c91765ba36a2f291157916bc05
ParkYeongseo <86962788+SkylightStudio07@users.noreply.github.com>
2026-05-03 00:40:51 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    string a;
+    string b;
+    
+    for(int i = 0; i < num_list.size(); i++) {
+        if(num_list[i] % 2 == 1) {
+            a += to_string(num_list[i]);
+        }
+        else if(num_list[i] % 2 == 0) {
+            b += to_string(num_list[i]);
+        }
+    }
+    
+    answer = stoi(a) + stoi(b);
+    
+    return answer;
+}
